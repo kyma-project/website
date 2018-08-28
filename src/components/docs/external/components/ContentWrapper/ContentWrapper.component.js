dd9c94338085385bfa4c1ced151625219b667f5d
Michał Hudy <michal@hudy.pl>
2018-08-28 15:43:03 +0200

    Remove `content.description` from documentation

--- src/components/docs/external/components/ContentWrapper/ContentWrapper.component.js
+++ src/components/docs/external/components/ContentWrapper/ContentWrapper.component.js
@@ -90,7 +90,6 @@ const ContentWrapper = ({ content }) => {
                   </div>
                 );
               })}
-            <Text>{content.description}</Text>
           </ContentDescription>
         </Wrapper>
       )}
