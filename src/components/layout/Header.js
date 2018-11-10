1abb52b2b825f5e4d9ec9d664e35127447e6f806
Mateusz Puczyński <mati6095@gmail.com>
2018-11-10 02:15:38 +0100

    Revert "Add functionality for a banner to promote different event where Kyma is present (#96)" (#110)
    This reverts commit 850579f09875d81e6002afa50076fbc6872fd44f.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Link from "gatsby-link";
 import ui from "../../locales/en/UI.json";
-import bannerData from "../../banner/banners.json";
+
 import NavigationContainer from "./Navigation.container";
-import Banner from "./Banner/Banner";
+import Banner from "./Banner";
 
 const Header = ({ headerClassName, logoClassName }) => {
   return (
     <>
-      <Banner slides={bannerData.slides} duration={bannerData.bannerDuration} />
+      <Banner />
       <header className={`bg-h ${headerClassName ? headerClassName : ""}`}>
         <div className="container">
           <Link className="home-link" to="/">
