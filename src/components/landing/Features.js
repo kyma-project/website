31b9bab25f6b9526a05d7b1842d5c2fd0cd9b5b6
Lukasz Gornicki <derberg@wp.pl>
2018-11-29 11:04:24 +0100

    Style fix after review

--- src/components/landing/Features.js
+++ src/components/landing/Features.js
@@ -27,13 +27,13 @@ const Features = () => {
   const sections = paragraphs;
   return (
     <div className="row kyma-attr row--sm-align-center">
-      <Link className="col-12 col-md-12" to={url}>
+      <Link className="col-12 col-md-12 link-sig" to={url}>
         <h2>{headline}</h2>
       </Link>
       {sections.map(section => {
         return (
           <section className="col-4 col-md-12" key={section.id}>
-            <Link to={section.url}>
+            <Link className="link-sig" to={section.url}>
               <h3>{section.headline}</h3>
             </Link>
             <div className="img-wrap">
