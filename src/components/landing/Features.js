220f3554a0ade1822aa59fa3425d16c6933ccf71
Lukasz Gornicki <derberg@wp.pl>
2018-11-30 11:52:15 +0100

    Modify descriptions on landing page (#141)

--- src/components/landing/Features.js
+++ src/components/landing/Features.js
@@ -1,40 +1,45 @@
 import React from "react";
+import Link from "gatsby-link";
 import landingPage from "../../locales/en/LandingPage.json";
-
 import Connectivity from "./features/Connectivity";
 import Extendable from "./features/Extendable";
 import TechIndependent from "./features/TechIndependent";
 
 const Features = () => {
+  const sectionPrefix = "features";
+  const headline = landingPage[sectionPrefix].headline;
+  const url = landingPage[sectionPrefix].url;
+  const paragraphs = landingPage[sectionPrefix].paragraphs || [];
+
   const getIllustrationComponent = sectionId => {
     switch (sectionId) {
       case "applicationConnector":
         return <Connectivity />;
       case "serverless":
         return <TechIndependent />;
-      case "serviceCatalog":
+      case "serviceManagement":
         return <Extendable />;
       default:
         return null;
     }
   };
 
-  const sections = landingPage.features || [];
+  const sections = paragraphs;
   return (
     <div className="row kyma-attr row--sm-align-center">
-      <h2 className="col-12 col-md-12">Key features</h2>
+      <Link className="col-12 col-md-12 link-sig" to={url}>
+        <h2>{headline}</h2>
+      </Link>
       {sections.map(section => {
         return (
           <section className="col-4 col-md-12" key={section.id}>
-            <h3>{section.headline}</h3>
+            <Link className="link-sig" to={section.url}>
+              <h3>{section.headline}</h3>
+            </Link>
             <div className="img-wrap">
               {getIllustrationComponent(section.id)}
             </div>
-            <ul className="list-disc">
-              {section.bulletPoints.map((bulletPoint, idx) => (
-                <li key={idx}>{bulletPoint}</li>
-              ))}
-            </ul>
+            <p>{section.paragraph}</p>
           </section>
         );
       })}
--- src/components/landing/WhatIs.js
+++ src/components/landing/WhatIs.js
@@ -1,38 +1,26 @@
 import React from "react";
+import Link from "gatsby-link";
 import landingPage from "../../locales/en/LandingPage.json";
-import ui from "../../locales/en/UI.json";
-
 import WhatIsSvg from "./features/WhatIs";
 
 const WhatIs = () => {
   const sectionPrefix = "whatIs";
   const headline = landingPage[sectionPrefix].headline;
+  const url = landingPage[sectionPrefix].url;
+  const action = landingPage[sectionPrefix].action;
   const paragraphs = landingPage[sectionPrefix].paragraphs || [];
-
-  const github = {
-    name: ui.socialMedia.github.name,
-    url: ui.socialMedia.github.url,
-  };
 
   return (
     <div className="row row--space-between row--sm-align-center">
       <div className="col-6 col-md-9 col-sm-12 align--center">
         <h2>{headline}</h2>
         <div>
-          {paragraphs.map((paragraph, idx) => <p key={idx}>{paragraph}</p>)}
-          <a href={github.url} className="btn btn-github">
-            <svg
-              className="sprite-icon sprite-icon--25 sprite-icon--inverse"
-              role="img"
-              aria-labelledby="buttonGithub"
-            >
-              <title id="buttonGithub">{github.name}</title>
-              <use xlinkHref="#github" />
-            </svg>
-            <span>
-              {landingPage.whatIs.action.replace("{{site}}", github.name)}
-            </span>
-          </a>
+          {paragraphs.map((paragraph, idx) => (
+            <p key={idx}>{paragraph}</p>
+          ))}
+          <Link className="btn btn-github" to={url}>
+            <span>{action}</span>
+          </Link>
         </div>
       </div>
       <div className="col-6 col-md-9 col-sm-12">
