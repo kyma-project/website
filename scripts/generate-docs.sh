c54487dd037b6d6b3a2aa2a5713e722390e5c06e
Agata Kucharska <agata.kucharska@sap.com>
2018-12-06 09:39:56 +0100

    Fix in makefile

---
+++ scripts/generate-docs.sh
@@ -0,0 +1,152 @@
+#!/usr/bin/env bash
+#
+# Script for generating and publishing new documentation to kyma-project.io
+
+pushd "$(pwd)" > /dev/null || exit
+
+on_error() {
+    echo -e "${RED}✗ Failed${NC}"
+    exit 1
+}
+trap on_error ERR
+
+on_exit() {
+    popd > /dev/null || exit
+}
+trap on_exit EXIT
+
+readonly SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly KYMA_REPOSITORY="https://github.com/kyma-project/kyma.git"
+readonly DOCUMENTATION_DIR="${SCRIPTS_DIR}/../static/documentation"
+readonly TEST_FILE_DIR="${DOCUMENTATION_DIR}/testfile.txt"
+readonly GENERATOR_IMAGE="eu.gcr.io/kyma-project/documentation-generator:0.1.49"
+
+# Colors
+readonly ARGS=("$@")
+readonly RED='\033[0;31m'
+readonly GREEN='\033[0;32m'
+readonly YELLOW='\033[0;33m'
+readonly NC='\033[0m' # No Color
+
+pass() {
+    local message="$1"
+    echo -e "${GREEN}√ ${message}${NC}"
+}
+
+step() {
+    local message="$1"
+    echo -e "\\n${YELLOW}${message}${NC}"
+}
+
+init() {
+    PUBLISH='false'
+    SSH_FILE=
+    OVERWRITE=
+
+    while test $# -gt 0; do
+        case "$1" in
+            --publish | -p)
+                PUBLISH='true'
+                shift
+                ;;
+            --ssh-file | -s)
+                shift
+                SSH_FILE=$1
+                shift
+                ;;
+            --skip | -sk)
+                shift
+                if (${1} -eq 'true') then
+                    echo 'Aborting the build to prevent a loop'
+                    exit 0
+                fi
+                shift
+                ;;
+            --overwrite-git-config)
+                shift
+                OVERWRITE=$1
+                shift
+                ;;
+            --help | -h)
+                exit 0
+                ;;
+            *)
+                echo "$1 is not a recognized flag!"
+                exit 1
+                ;;
+        esac
+    done
+    readonly PUBLISH
+    readonly SSH_FILE
+    readonly OVERWRITE
+}
+
+generate() {
+    docker run --rm -v "${DOCUMENTATION_DIR}:/app/documentation" \
+               -e APP_OUTPUT="/app/documentation" \
+               -e APP_DOC_CONFIG_FILE="/app/documentation/config.json" \
+               -e APP_TOKEN="${BOT_GITHUB_TOKEN}" \
+               ${GENERATOR_IMAGE} || return
+}
+
+publish() {
+    if $OVERWRITE; then
+        # make ssh dir
+        mkdir /root/.ssh/
+
+        # create known_hosts file
+        touch /root/.ssh/known_hosts
+
+        # add github to known_hosts
+        ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+
+        # key need to be only readable
+        chmod 400 $SSH_FILE
+
+        # create a authentication agent
+        eval `ssh-agent -s`
+
+        # add ssh-key
+        ssh-add $SSH_FILE
+        ssh-add -l
+
+        # configure git
+        sh ./scripts/helpers/git-config.sh -s $SSH_FILE
+    fi
+
+    # echo "Detecting changes"
+    # local changes
+    # changes=$(git status --porcelain "${DOCUMENTATION_DIR}" | wc -w)
+    # if [[ "${changes}" -eq 0 ]]; then
+    #     echo "Nothing to publish"
+    #     return
+    # fi
+
+    echo "Commit documentation"
+    touch ${TEST_FILE_DIR}
+    echo "Test file" > ${TEST_FILE_DIR}
+    git add "${TEST_FILE_DIR}" || return
+    git add "${DOCUMENTATION_DIR}" || return
+
+    git remote add origin git@github.com:kyma-project/website.git
+    git commit -m "Publish documentation for Kyma" --no-verify --author=<Kyma Bot <kyma.bot@sap.com>>
+    echo "Pushing documentation to testing-prow"
+    git push origin HEAD:testing-prow
+}
+
+main() {
+    init "${ARGS[@]}" || return
+
+    step "Generating"
+    generate || return
+    pass "Generated"
+
+    step "Publishing"
+    if [[ ${PUBLISH} == true ]]; then
+        publish || return
+    else
+        echo "Skipped"
+    fi
+    pass "Published"
+}
+main
---
+++ scripts/helpers/git-config.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+SSH_FILE=
+
+while test $# -gt 0; do
+    case "$1" in
+        --ssh-file | -s)
+            shift
+            SSH_FILE=$1
+            shift
+            ;;
+        *)
+            echo "$1 is not a recognized flag!"
+            exit 1;
+            ;;
+    esac
+done
+readonly SSH_FILE
+
+# configure git
+git config --global user.email "kyma.bot@sap.com"
+git config --global user.name "Kyma Bot"
+git config --global core.sshCommand 'ssh -i '$SSH_FILE''
