68b3549173115a63df5c14ec2f5f0d79728a531d
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-02-21 10:49:13 +0100

    Add navigation components

---
+++ src/components/docs/navigation/leftNavigation/NavigationList/NavigationGroup.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+
+import NavigationSections from "./NavigationSection";
+
+import {
+  DocsNavigationTopic,
+  DocsContentItem,
+  DocsManifestItem,
+} from "@components/docs/types";
+import {
+  ActiveNav,
+  ScrollSpyActiveNodes,
+} from "@components/docs/navigation/types";
+
+import {
+  NavigationGroupContainer,
+  NavigationHeader,
+  NavigationItems,
+  NavigationItem,
+  LinkWrapper,
+  NavigationLink,
+  NavigationSectionArrow,
+} from "./styled";
+
+interface NavigationGroupProps {
+  title?: string;
+  groupType: string;
+  topics: DocsNavigationTopic[];
+  items: DocsManifestItem[];
+  content: DocsContentItem;
+  setActiveNav: Function;
+  isLinkActive: Function;
+  getPathLink: Function;
+  activeNav: ActiveNav;
+  activeNodes: ScrollSpyActiveNodes;
+  hideNavIfShouldOnMobile: Function;
+}
+
+const NavigationGroup: React.FunctionComponent<NavigationGroupProps> = ({
+  title = "",
+  groupType,
+  items,
+  topics,
+  content,
+  setActiveNav,
+  isLinkActive,
+  getPathLink,
+  activeNav,
+  activeNodes,
+  hideNavIfShouldOnMobile,
+  ...otherProps
+}) => {
+  const renderArrow = (item: DocsManifestItem) => (
+    <NavigationSectionArrow
+      onClick={() => {
+        setActiveNav({
+          id: item.id,
+          type: groupType,
+          hash: "",
+        });
+      }}
+      activeArrow={item.id === activeNav.id}
+      active={isLinkActive({
+        id: item.id,
+        type: groupType,
+      })}
+    />
+  );
+
+  const renderNavigationItem = (item: DocsManifestItem) => {
+    const topic: DocsNavigationTopic | undefined =
+      topics && topics.find(obj => obj.id === item.id);
+
+    return (
+      <NavigationItem key={item.id}>
+        <LinkWrapper>
+          {topic && topic.sections && renderArrow(item)}
+          <NavigationLink
+            active={isLinkActive({
+              id: item.id,
+              type: groupType,
+            })}
+            to={getPathLink({
+              id: item.id,
+              type: groupType,
+              hash: "",
+            })}
+            onClick={() => hideNavIfShouldOnMobile(topics && topics.length > 0)}
+          >
+            {item.displayName}
+          </NavigationLink>
+        </LinkWrapper>
+        {topic && topic.sections && (
+          <NavigationSections
+            items={topic.sections}
+            groupType={groupType}
+            rootId={item.id}
+            content={content}
+            isLinkActive={isLinkActive}
+            activeNav={activeNav}
+            activeNodes={activeNodes}
+            getPathLink={getPathLink}
+            setActiveNav={setActiveNav}
+            hideNavIfShouldOnMobile={hideNavIfShouldOnMobile}
+          />
+        )}
+      </NavigationItem>
+    );
+  };
+
+  return (
+    <NavigationGroupContainer>
+      {title && <NavigationHeader>{title}</NavigationHeader>}
+      <NavigationItems showAll>
+        {items && items.map(item => renderNavigationItem(item))}
+      </NavigationItems>
+    </NavigationGroupContainer>
+  );
+};
+
+export default NavigationGroup;
---
+++ src/components/docs/navigation/leftNavigation/NavigationList/NavigationSection.tsx
@@ -0,0 +1,178 @@
+import React from "react";
+
+import {
+  DocsNavigationTopicSection,
+  DocsContentItem,
+} from "@components/docs/types";
+import {
+  ActiveNav,
+  ScrollSpyActiveNodes,
+} from "@components/docs/navigation/types";
+
+import {
+  NavigationItems,
+  NavigationItem,
+  LinkWrapper,
+  NavigationLink,
+  NavigationSectionArrow,
+} from "./styled";
+
+interface NavigationSectionsProps {
+  items?: DocsNavigationTopicSection[];
+  groupType: string;
+  rootId?: string;
+  parentId?: string;
+  content: DocsContentItem;
+  setActiveNav: Function;
+  isLinkActive: Function;
+  getPathLink: Function;
+  activeNav: ActiveNav;
+  activeNodes: ScrollSpyActiveNodes;
+  hideNavIfShouldOnMobile: Function;
+}
+
+const NavigationSections: React.FunctionComponent<NavigationSectionsProps> = ({
+  items = [],
+  rootId = "",
+  parentId = "",
+  groupType,
+  content,
+  setActiveNav,
+  isLinkActive,
+  getPathLink,
+  activeNav,
+  activeNodes,
+  hideNavIfShouldOnMobile,
+}) => {
+  const hashing = (item: DocsNavigationTopicSection) => {
+    if (parentId) return `${parentId}-${item.anchor}`;
+
+    const topicType = item.topicType
+      ? item.topicType.replace(/ /g, "-").toLowerCase()
+      : item.anchor;
+    return `${topicType}-${item.anchor}`;
+  };
+
+  const renderArrow = (
+    hash: string,
+    isActive: boolean,
+    isActiveNavArrow: boolean,
+  ) => (
+    <NavigationSectionArrow
+      onClick={() => {
+        setActiveNav({
+          id: rootId,
+          type: groupType,
+          hash: hash,
+        });
+      }}
+      activeArrow={isActiveNavArrow}
+      active={isActive}
+    />
+  );
+
+  const renderNavigationItem = (item: DocsNavigationTopicSection) => {
+    const hash = hashing(item);
+    const hasSubElements = item && item.titles && item.titles.length > 0;
+
+    let isActive = false;
+    let isActiveNavArrow = false;
+
+    if (content.id === rootId) {
+      if (parentId) {
+        isActive = Boolean(
+          activeNodes &&
+            (activeNodes.document && activeNodes.document.id === hash),
+        );
+      } else {
+        isActive = Boolean(
+          activeNodes &&
+            (activeNodes.groupOfDocuments &&
+              activeNodes.groupOfDocuments.id === hash),
+        );
+      }
+
+      if (
+        activeNodes &&
+        activeNodes.groupOfDocuments &&
+        activeNodes.groupOfDocuments.id.startsWith(hash)
+      ) {
+        isActiveNavArrow = true;
+      }
+    }
+
+    if (!isActiveNavArrow) {
+      isActiveNavArrow = Boolean(
+        hasSubElements &&
+          activeNav.id === rootId &&
+          activeNav.hash &&
+          activeNav.hash.startsWith(item.anchor),
+      );
+    }
+
+    const key = parentId
+      ? `${rootId}-${parentId}-${item.anchor}`
+      : `${rootId}-${item.anchor}`;
+
+    return (
+      <NavigationItem key={key}>
+        <LinkWrapper>
+          {hasSubElements && renderArrow(hash, isActive, isActiveNavArrow)}
+          <NavigationLink
+            active={isActive}
+            to={getPathLink({
+              id: rootId,
+              type: groupType,
+              hash: hash,
+            })}
+            onClick={() => hideNavIfShouldOnMobile(true)}
+            borderLeft={parentId || !hasSubElements}
+          >
+            {item.name}
+          </NavigationLink>
+        </LinkWrapper>
+        {hasSubElements && (
+          <NavigationSections
+            items={item.titles}
+            groupType={groupType}
+            rootId={rootId}
+            parentId={item.anchor}
+            content={content}
+            isLinkActive={isLinkActive}
+            activeNav={activeNav}
+            activeNodes={activeNodes}
+            getPathLink={getPathLink}
+            setActiveNav={setActiveNav}
+            hideNavIfShouldOnMobile={hideNavIfShouldOnMobile}
+          />
+        )}
+      </NavigationItem>
+    );
+  };
+
+  let isActiveNav = false;
+  if (
+    parentId &&
+    activeNodes &&
+    activeNodes.groupOfDocuments &&
+    activeNodes.groupOfDocuments.id.startsWith(parentId)
+  ) {
+    isActiveNav = content.id === rootId;
+  } else {
+    isActiveNav = !parentId ? content.id === rootId : false;
+  }
+
+  const isClickedNav = parentId
+    ? activeNav.id === rootId &&
+      activeNav.hash &&
+      activeNav.hash.startsWith(parentId)
+    : activeNav.id === rootId;
+
+  return (
+    <NavigationItems secondary marginTop show={isActiveNav || isClickedNav}>
+      {items.map(item => renderNavigationItem(item))}
+    </NavigationItems>
+  );
+};
+
+export default NavigationSections;
