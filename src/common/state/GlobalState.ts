231e8e6d17331b33b09244d567b98820e3464457
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-03-04 09:14:22 +0100

    Remove Toasts hooks

--- src/common/state/GlobalState.ts
+++ src/common/state/GlobalState.ts
@@ -2,10 +2,8 @@ import createContainer from "constate";
 
 import { nest } from "./nest";
 
-import { useToasts } from "./useToasts";
 import { usePopup } from "./usePopup";
 
-export const ToastsState = createContainer(useToasts);
 export const PopupState = createContainer(usePopup);
 
-export const GlobalState = nest(ToastsState.Provider, PopupState.Provider);
+export const GlobalState = nest(PopupState.Provider);
