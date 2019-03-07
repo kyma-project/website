6f4557881e4c7108e1ca95fc8b51cb8baa6d59d8
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-03-07 11:19:36 +0100

    Add comment for blankLinesRegex

--- src/components/shared/ReactMarkdown/helpers.ts
+++ src/components/shared/ReactMarkdown/helpers.ts
@@ -1,4 +1,5 @@
 const tabsBlockRegex = /<div tabs>(.|\n)*?<\/div>/gm;
+// Regex for removing blank lines for correct parsing toggle in ReactMarkdown component
 const blankLinesRegex = /^\s*$(?:\r\n?|\n)/gm;
 
 export const removeBlankLines = (source: string) =>
