3701b979e601a0ee06034bd0f7119a66505afba0
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-04-05 14:02:54 +0200

    Fix bug with "<" char in code blocks

--- src/components/shared/ReactMarkdown/helpers.ts
+++ src/components/shared/ReactMarkdown/helpers.ts
@@ -1,14 +1,18 @@
 const tabsBlockRegex = /<div tabs>(.|\n)*?<\/div>/gm;
 // Regex for removing blank lines for correct parsing toggle in ReactMarkdown component
 const blankLinesRegex = /^\s*$(?:\r\n?|\n)/gm;
+const codeBlocksRegex = /^(([ \t]*`{3,4})([^\n]*)([\s\S]+?)(^[ \t]*\2))/gm;
+const lessThanChar = "&#60;";
 
 export const removeBlankLines = (source: string) =>
   source.replace(blankLinesRegex, "");
 
 export const removeBlankLinesFromTabsBlock = (source?: string) =>
   source &&
-  source.replace(tabsBlockRegex, occurrence => {
-    const result = removeBlankLines(occurrence);
+  source.replace(tabsBlockRegex, occurrence => removeBlankLines(occurrence));
 
-    return result;
-  });
+export const replaceAllLessThanChars = (source?: string) =>
+  source &&
+  source.replace(codeBlocksRegex, occurrence =>
+    occurrence.replace(/</gm, lessThanChar),
+  );
