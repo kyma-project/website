05c8a4a9b557c34480305329724663369a08fc7c
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-05-02 12:40:10 +0200

    Run lint-fix

--- netlify/functions/kyma-organization-github-hook/index.ts
+++ netlify/functions/kyma-organization-github-hook/index.ts
@@ -1,5 +1,5 @@
 import { Handler, Context, Callback, APIGatewayEvent } from "aws-lambda";
-import { Issues } from "github-webhook-event-types"
+import { Issues } from "github-webhook-event-types";
 
 interface Response {
   statusCode: number;
@@ -13,7 +13,7 @@ const handler: Handler = (
 ) => {
   console.log(event.headers);
 
-  const issue = JSON.parse(event.body) as any as Issues;
+  const issue = (JSON.parse(event.body) as any) as Issues;
   console.log(issue.action);
   console.log(issue.changes);
 
