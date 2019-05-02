5b4bf343d43e17e207a748011da63a6c5ab6682a
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-05-02 17:16:36 +0200

    Update function

--- netlify/functions/kyma-organization-github-hook/issues.event.ts
+++ netlify/functions/kyma-organization-github-hook/issues.event.ts
@@ -14,6 +14,8 @@ enum IssuesActionType {
 const EPIC_LABEL = "Epic";
 
 export const checkIssuesEvent = (event: Issues): boolean => {
+  console.log("action: ", event.action);
+
   switch (event.action as IssuesActionType) {
     case (IssuesActionType.OPENED,
     IssuesActionType.EDITED,
@@ -23,6 +25,7 @@ export const checkIssuesEvent = (event: Issues): boolean => {
       return checkLabelsFromChangedIssue(event.issue.labels);
     case (IssuesActionType.LABELED, IssuesActionType.UNLABELED): {
       const label = (event as any).label;
+      console.log("label: ", label);
       return label ? checkChangedLabel(label) : false;
     }
     default:
