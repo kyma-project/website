6ebb4ad11b889ec1a6b90839d7e305ee7f35ea78
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-05-17 21:05:56 +0200

    Fix cookiebanner

--- src/components/layout/CookiesBanner.tsx
+++ src/components/layout/CookiesBanner.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Cookies } from "react-cookie";
 
 import Link from "@components/shared/Link";
 import Icon from "@components/shared/Icon";
@@ -17,27 +18,40 @@ const gt = getTranslation("cookies");
 
 const CookiesMessageBanner: React.FunctionComponent<IntlInterface> = ({
   formatMessage,
-}) => (
-  <StyledCookieBanner
-    disableStyle={true}
-    dismissOnScroll={false}
-    message={formatMessage({ id: "message" })}
-    link={
-      <>
-        <FormattedMessage id={gt("readPrivacyStatement")}>
-          {readPrivacyStatement => (
-            <ReadPrivacyStatementLink
-              to={config.links.SAP_PRIVACY_LINK}
-              externalIcon={true}
-            >
-              {readPrivacyStatement}
-            </ReadPrivacyStatementLink>
-          )}
-        </FormattedMessage>
-      </>
-    }
-    buttonMessage={formatMessage({ id: "closeButton" })}
-  />
-);
+}) => {
+  const cookies = new Cookies();
+  const [init, setInit] = useState<boolean>(false);
+
+  useEffect(() => {
+    setInit(true);
+  }, []);
+
+  if (!init || !cookies || cookies.get("accepts-cookies")) {
+    return null;
+  }
+
+  return (
+    <StyledCookieBanner
+      disableStyle={true}
+      dismissOnScroll={false}
+      message={formatMessage({ id: "message" })}
+      link={
+        <>
+          <FormattedMessage id={gt("readPrivacyStatement")}>
+            {readPrivacyStatement => (
+              <ReadPrivacyStatementLink
+                to={config.links.SAP_PRIVACY_LINK}
+                externalIcon={true}
+              >
+                {readPrivacyStatement}
+              </ReadPrivacyStatementLink>
+            )}
+          </FormattedMessage>
+        </>
+      }
+      buttonMessage={formatMessage({ id: "closeButton" })}
+    />
+  );
+};
 
 export default injectIntl("cookies")(CookiesMessageBanner);
