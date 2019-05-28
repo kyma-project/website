42de3b14a0c715f54729affee521d1135aad5673
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-05-28 10:00:23 +0200

    Add Saas to early adopters

--- src/components/landingPage/EarlyAdopters/EarlyAdoptersListItem.tsx
+++ src/components/landingPage/EarlyAdopters/EarlyAdoptersListItem.tsx
@@ -1,22 +1,30 @@
 import React from "react";
 import { StyledEarlyAdoptersListItem } from "../styled";
 
+import SaasLogo from "@static/img/early-adopters/saas.svg";
+
 interface ListItemProps {
   company: string;
 }
 export const EarlyAdoptersListItem: React.FunctionComponent<ListItemProps> = ({
   company,
   children,
-}) => (
-  <StyledEarlyAdoptersListItem>
-    <svg
-      className={`sprite-icon sprite-icon--${company}`}
-      role="img"
-      aria-labelledby={company}
-    >
-      {children}
-      <title id={company}>{company}</title>
-      <use xlinkHref={`#${company}`} />
-    </svg>
-  </StyledEarlyAdoptersListItem>
-);
+}) => {
+  if (company === "saas") {
+    return <img src={SaasLogo} alt="SAAS - Software as a Service" />;
+  }
+
+  return (
+    <StyledEarlyAdoptersListItem>
+      <svg
+        className={`sprite-icon sprite-icon--${company}`}
+        role="img"
+        aria-labelledby={company}
+      >
+        {children}
+        <title id={company}>{company}</title>
+        <use xlinkHref={`#${company}`} />
+      </svg>
+    </StyledEarlyAdoptersListItem>
+  );
+};
