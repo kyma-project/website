e3131096bbab4a8f914820eca2bac10b0cdc517d
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-06-04 08:51:48 +0200

    After review v2

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -7,9 +7,6 @@ export const DOCS_RESPONSIVE_BREAKPOINT: number = 768;
 export const RICK_ROLLED = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 
 export const ALGOLIA = {
-  API_KEY:
-    "e55c0e91d299036ba9171a8961e05c88" ||
-    process.env.ALGOLIA_API_KEY ||
-    "25626fae796133dc1e734c6bcaaeac3c",
-  INDEX_NAME: "kyma-project" || process.env.ALGOLIA_INDEX_NAME || "docsearch",
+  API_KEY: process.env.ALGOLIA_API_KEY || "25626fae796133dc1e734c6bcaaeac3c",
+  INDEX_NAME: process.env.ALGOLIA_INDEX_NAME || "docsearch",
 };
