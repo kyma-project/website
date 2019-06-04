57ddb0bcc546e2b1857a397918d6f23290c6203e
Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>
2019-06-04 21:43:28 +0200

    Add links to the logos in the early adopters section (#348)

--- src/components/landingPage/EarlyAdopters/EarlyAdopters.tsx
+++ src/components/landingPage/EarlyAdopters/EarlyAdopters.tsx
@@ -15,44 +15,61 @@ import Sprites, { getSapGradientDef, getTwiggleGradientDef } from "../Sprites";
 
 import { FormattedMessage, getTranslation } from "@common/i18n";
 
+export interface EarlyAdopter {
+  company: string;
+  title: string;
+  link: string;
+}
+
+interface Props {
+  earlyAdopters: EarlyAdopter[];
+}
+
 const gt = getTranslation("landingPage.earlyAdopters");
 
-const EarlyAdopters: React.FunctionComponent = () => (
-  <EarlyAdoptersWrapper>
-    <Sprites />
-    <Grid.Container as="section">
-      <Grid.Row>
-        <Grid.Unit df={12}>
-          <FormattedMessage id={gt("headline")}>
-            {headline => (
-              <H as="h2" center={true}>
-                {headline}
-              </H>
-            )}
-          </FormattedMessage>
-        </Grid.Unit>
-        <Grid.Unit df={12}>
-          <EarlyAdoptersContent className="adopters">
-            <EarlyAdoptersList>
-              <EarlyAdoptersListItem company="twiggle">
-                {getTwiggleGradientDef()}
-              </EarlyAdoptersListItem>
-              <EarlyAdoptersListItem company="netconomy" />
-              <EarlyAdoptersListItem company="sap">
-                {getSapGradientDef()}
-              </EarlyAdoptersListItem>
-              <EarlyAdoptersListItem company="mgm" />
-              <EarlyAdoptersListItem company="accenture-interactive" />
-            </EarlyAdoptersList>
-            <EarlyAdoptersList>
-              <EarlyAdoptersListItem company="arithnea" />
-              <EarlyAdoptersListItem company="saas" />
-            </EarlyAdoptersList>
-          </EarlyAdoptersContent>
-        </Grid.Unit>
-      </Grid.Row>
-    </Grid.Container>
-  </EarlyAdoptersWrapper>
-);
+const EarlyAdopters: React.FunctionComponent<Props> = ({ earlyAdopters }) => {
+  const getCompanyData = (company: string) =>
+    earlyAdopters.find(c => c.company === company) as EarlyAdopter;
+
+  return (
+    <EarlyAdoptersWrapper>
+      <Sprites />
+      <Grid.Container as="section">
+        <Grid.Row>
+          <Grid.Unit df={12}>
+            <FormattedMessage id={gt("headline")}>
+              {headline => (
+                <H as="h2" center={true}>
+                  {headline}
+                </H>
+              )}
+            </FormattedMessage>
+          </Grid.Unit>
+          <Grid.Unit df={12}>
+            <EarlyAdoptersContent className="adopters">
+              <EarlyAdoptersList>
+                <EarlyAdoptersListItem {...getCompanyData("twiggle")}>
+                  {getTwiggleGradientDef()}
+                </EarlyAdoptersListItem>
+                <EarlyAdoptersListItem {...getCompanyData("netconomy")} />
+                <EarlyAdoptersListItem {...getCompanyData("sap")}>
+                  {getSapGradientDef()}
+                </EarlyAdoptersListItem>
+                <EarlyAdoptersListItem {...getCompanyData("mgm")} />
+                <EarlyAdoptersListItem
+                  {...getCompanyData("accenture-interactive")}
+                />
+              </EarlyAdoptersList>
+              <EarlyAdoptersList>
+                <EarlyAdoptersListItem {...getCompanyData("arithnea")} />
+                <EarlyAdoptersListItem {...getCompanyData("saas")} />
+              </EarlyAdoptersList>
+            </EarlyAdoptersContent>
+          </Grid.Unit>
+        </Grid.Row>
+      </Grid.Container>
+    </EarlyAdoptersWrapper>
+  );
+};
 
 export default EarlyAdopters;
--- src/components/landingPage/EarlyAdopters/EarlyAdoptersListItem.tsx
+++ src/components/landingPage/EarlyAdopters/EarlyAdoptersListItem.tsx
@@ -1,34 +1,40 @@
 import React from "react";
 import { StyledEarlyAdoptersListItem } from "../styled";
 
+import Link from "@components/shared/Link";
+import Tooltip from "@components/shared/Tooltip";
+
 import SaasLogo from "@static/img/early-adopters/saas.svg";
 
-interface ListItemProps {
-  company: string;
-}
-export const EarlyAdoptersListItem: React.FunctionComponent<ListItemProps> = ({
+import { EarlyAdopter } from "./EarlyAdopters";
+
+export const EarlyAdoptersListItem: React.FunctionComponent<EarlyAdopter> = ({
   company,
+  title,
+  link,
   children,
 }) => {
+  let content = (
+    <svg
+      className={`sprite-icon sprite-icon--${company}`}
+      role="img"
+      aria-labelledby={company}
+    >
+      {children}
+      <title id={company}>{company}</title>
+      <use xlinkHref={`#${company}`} />
+    </svg>
+  );
+
   if (company === "saas") {
-    return (
-      <StyledEarlyAdoptersListItem>
-        <img src={SaasLogo} alt="SAAS - Software as a Service" />
-      </StyledEarlyAdoptersListItem>
-    );
+    content = <img src={SaasLogo} alt="SAAS - Software as a Service" />;
   }
 
   return (
-    <StyledEarlyAdoptersListItem>
-      <svg
-        className={`sprite-icon sprite-icon--${company}`}
-        role="img"
-        aria-labelledby={company}
-      >
-        {children}
-        <title id={company}>{company}</title>
-        <use xlinkHref={`#${company}`} />
-      </svg>
-    </StyledEarlyAdoptersListItem>
+    <Tooltip content={title} placement="bottom">
+      <StyledEarlyAdoptersListItem>
+        {link ? <Link.External to={link}>{content}</Link.External> : content}
+      </StyledEarlyAdoptersListItem>
+    </Tooltip>
   );
 };
