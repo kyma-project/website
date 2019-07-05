a547cedbdfccd10e6e0d28c81120c0a5fbdb8ad9
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-07-05 10:04:58 +0200

    Add RSS and JSON Feed to website

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,6 +91,123 @@ module.exports = {
         whitelist: ["GOOGLE_CSE", "ALGOLIA_API_KEY", "ALGOLIA_INDEX_NAME"],
       },
     },
+    {
+      resolve: "gatsby-plugin-feed-generator",
+      options: {
+        generator: `GatsbyJS`,
+        rss: true, // Set to false to stop rss generation
+        json: true, // Set to false to stop json feed generation
+        siteQuery: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+            }
+          }
+        }
+      `,
+        feeds: [
+          {
+            name: "feed", // This determines the name of your feed file => feed.json & feed.xml
+            query: `
+            {
+              allMarkdownRemark(sort: {order: DESC, fields: [fields___date]}) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    fields {
+                      slug
+                      date
+                    }
+                    frontmatter {
+                      title
+                    }
+                  }
+                }
+              }
+            }            
+          `,
+            normalize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges
+                .filter(arg => arg.node.fields.slug.startsWith("/blog/"))
+                .map(edge => {
+                  return Object.assign({}, edge.node.frontmatter, {
+                    description: edge.node.excerpt,
+                    url:
+                      site.siteMetadata.siteUrl +
+                      edge.node.fields.slug.slice(1),
+                    date: edge.node.fields.date,
+                    guid:
+                      site.siteMetadata.siteUrl +
+                      edge.node.fields.slug.slice(1),
+                  });
+                });
+            },
+          },
+        ],
+      },
+    },
   ],
   pathPrefix: "/website",
 };
+
+// {
+//   resolve: `gatsby-plugin-feed`,
+//   options: {
+//     query: `
+//       {
+//         site {
+//           siteMetadata {
+//             title
+//             description
+//             siteUrl
+
+//           }
+//         }
+//       }
+//     `,
+//     feeds: [
+//       {
+//         serialize: ({ query: { site, allMarkdownRemark } }) => {
+//           return allMarkdownRemark.edges.map(edge => {
+//             console.log(edge.node.fields.slug);
+//             return Object.assign({}, edge.node.frontmatter, {
+//               description: edge.node.excerpt,
+//               date: edge.node.frontmatter.date,
+//               url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+//               guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+//             });
+//           });
+//         },
+//         query: `
+//         {
+//           allMarkdownRemark {
+//             edges {
+//               node {
+//                 excerpt
+//                 html
+//                 fields {
+//                   slug
+//                 }
+//                 frontmatter {
+//                   title
+//                 }
+//               }
+//             }
+//           }
+//         }
+//         `,
+//         output: "/rss.xml",
+//         title: "Your Site's RSS Feed",
+//         // optional configuration to insert feed reference in pages:
+//         // if `string` is used, it will be used to create RegExp and then test if pathname of
+//         // current page satisfied this regular expression;
+//         // if not provided or `undefined`, all pages will have feed reference inserted
+//         match: ".*?blog.*?",
+//       },
+//     ],
+//   },
+// },
