9afcf2c91c89f76ab5f63bc4c879513d87773624
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-08-02 11:59:10 +0200

    Update content-loader tool for community view

---
+++ tools/content-loader/src/prepare-community/copy-community.ts
@@ -0,0 +1,26 @@
+import to from "await-to-js";
+import { VError } from "verror";
+
+import ClusterDocsTopicSerializer from "../cdt-serializer";
+
+export class CopyCommunity {
+  do = async (source: string, output: string) => {
+    console.log(`Copy community content to ${output}`);
+    const [err] = await to(this.copy(source, output));
+    if (err) {
+      throw new VError(err, `while copying documentation to ${output}`);
+    }
+  };
+
+  private copy = async (source: string, output: string) => {
+    const copyRegex: string = `([A-z0-9-_.&]*\.md|assets\/[A-z0-9-_.&]*\.(png|jpg|gif|jpeg|svg|yaml|yml|json))`;
+    const [err] = await to(
+      ClusterDocsTopicSerializer.do(source, output, copyRegex),
+    );
+    if (err) {
+      throw err;
+    }
+  };
+}
+
+export default new CopyCommunity();
