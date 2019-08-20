3eead589230edd0f5b8a4502663317b9c0e5c834
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-08-20 16:02:10 +0200

    Remove gatsby-plugin-optimize-svgs plugin

--- gatsby/config-api/plugins.ts
+++ gatsby/config-api/plugins.ts
@@ -45,7 +45,6 @@ export const plugins: Plugins = [
   },
   "gatsby-plugin-banner-slides-yml",
   "gatsby-plugin-early-adopters-yml",
-  "gatsby-plugin-optimize-svgs",
   {
     resolve: "gatsby-transformer-remark",
     options: {
