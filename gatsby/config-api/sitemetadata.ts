293859c2abb671cd41e88aadb9f651ca916aac29
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-10-01 09:54:50 +0200

    update image source for test

--- gatsby/config-api/sitemetadata.ts
+++ gatsby/config-api/sitemetadata.ts
@@ -2,7 +2,7 @@ import { SiteMetadata } from "./types";
 const configSiteMetadata: SiteMetadata = require("../../config").siteMetadata;
 
 export const siteMetadata: SiteMetadata = {
-  siteUrl: "https://kyma-project.io/",
+  siteUrl: "https://5d9300d72562c2000a6e13ff--kyma-project.netlify.com",
   twitterUsername: "kymaproject",
   ...configSiteMetadata,
 };
