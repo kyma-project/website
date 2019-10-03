1b0c72c147f1554becc52dc3280305f2456c72ba
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-10-03 10:55:03 +0200

    Change get-unique function

--- tools/content-loader/src/helpers/get-unique.ts
+++ tools/content-loader/src/helpers/get-unique.ts
@@ -1,9 +1,9 @@
 export const getUnique = <T extends { [key: string]: any }>(
   arr: T[],
   comp: string,
-) =>
-  arr
+): T[] => <T[]>arr
     .map(e => e[comp])
-    .map((e, i, final) => (final.indexOf(e) === i && i) as number)
-    .filter(e => arr[e])
-    .map(e => arr[e]);
+    .map((e, i, final) => final.indexOf(e) === i && i)
+    .filter(e => typeof e === "number" && arr[e])
+    .map(e => (typeof e === "number" ? arr[e] : undefined))
+    .filter(e => typeof e !== "undefined");
