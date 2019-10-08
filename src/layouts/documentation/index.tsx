f0eea2cdc59ccc3fb192f32e9c0b7e13486f0fa5
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-10-08 22:18:19 +0200

    Fetching swagger component on loading time docs

--- src/layouts/documentation/index.tsx
+++ src/layouts/documentation/index.tsx
@@ -1,11 +1,32 @@
 import React from "react";
+
+import { DocsPageContext, SpecificationType } from "@typings/docs";
+
 import { DefaultLayout } from "../default";
+
+async function loadSwagger(): Promise<void> {
+  // @ts-ignore
+  return import("swagger-ui-dist").then(() => null);
+}
 
 export const DocumentationLayout: React.FunctionComponent<any> = ({
   children,
   ...pageContext
-}) => (
-  <DefaultLayout {...pageContext} inDocsLayout={true}>
-    {children}
-  </DefaultLayout>
-);
+}) => {
+  const { specifications } = pageContext as DocsPageContext;
+  if (
+    specifications &&
+    specifications.length &&
+    specifications.some(
+      specification => specification.type === SpecificationType.OPEN_API,
+    )
+  ) {
+    loadSwagger();
+  }
+
+  return (
+    <DefaultLayout {...pageContext} inDocsLayout={true}>
+      {children}
+    </DefaultLayout>
+  );
+};
