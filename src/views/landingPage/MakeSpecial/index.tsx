4795f918ae66b48bb0d99fbe87f672efce9eacaf
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-10-21 15:04:51 +0200

    update texts

--- src/views/landingPage/MakeSpecial/index.tsx
+++ src/views/landingPage/MakeSpecial/index.tsx
@@ -17,44 +17,42 @@ export const MakeSpecial: React.FunctionComponent = () => (
     <Grid.Container as="section">
       <Grid.Row space={true} alignCenter={true}>
         <Grid.Unit df={6} lg={6} md={12}>
-          <FormattedMessage id={gt("extendEasily.headline")}>
+          <FormattedMessage id={gt("extend.headline")}>
             {headline => (
               <H as="h2" center={true}>
                 {headline}
               </H>
             )}
           </FormattedMessage>
-          <>
-            <FormattedMessage id={gt("extendEasily.paragraph")}>
-              {paragraph => <Paragraph>{paragraph}</Paragraph>}
-            </FormattedMessage>
-            {/* make sure this link is correct !!!!!!!!!! */}
-            <CenteredLink to="/docs">
-              <Button.Emphasized>
-                <FormattedMessage id={gt("extendEasily.readDocsButton")} />
-              </Button.Emphasized>
-            </CenteredLink>
-          </>
+
+          <FormattedMessage id={gt("extend.paragraph")}>
+            {paragraph => <Paragraph justify={true}>{paragraph}</Paragraph>}
+          </FormattedMessage>
+          {/* make sure this link is correct !!!!!!!!!! */}
+          <CenteredLink to="/docs/#overview-in-a-nutshell">
+            <Button.Emphasized>
+              <FormattedMessage id={gt("extend.readDocsButton")} />
+            </Button.Emphasized>
+          </CenteredLink>
         </Grid.Unit>
         <Grid.Unit df={6} lg={6} md={12}>
-          <FormattedMessage id={gt("rightTechnologies.headline")}>
+          <FormattedMessage id={gt("rightTools.headline")}>
             {headline => (
               <H as="h2" center={true}>
                 {headline}
               </H>
             )}
           </FormattedMessage>
-          <>
-            <FormattedMessage id={gt("rightTechnologies.paragraph")}>
-              {paragraph => <Paragraph>{paragraph}</Paragraph>}
-            </FormattedMessage>
-            {/* make sure this link is correct !!!!!!!!!! */}
-            <CenteredLink to="/docs">
-              <Button.Emphasized>
-                <FormattedMessage id={gt("rightTechnologies.readDocsButton")} />
-              </Button.Emphasized>
-            </CenteredLink>
-          </>
+
+          <FormattedMessage id={gt("rightTools.paragraph")}>
+            {paragraph => <Paragraph justify={true}>{paragraph}</Paragraph>}
+          </FormattedMessage>
+          {/* make sure this link is correct !!!!!!!!!! */}
+          <CenteredLink to="/docs/#details-components">
+            <Button.Emphasized>
+              <FormattedMessage id={gt("rightTools.readDocsButton")} />
+            </Button.Emphasized>
+          </CenteredLink>
         </Grid.Unit>
       </Grid.Row>
       <UsedProjectIcons />
