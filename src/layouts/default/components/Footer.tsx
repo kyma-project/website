2205466acc9f86461842ee0791d845bffe2c4824
Valentin Vieriu <valentin.vieriu@sap.com>
2020-05-26 17:35:25 +0200

    added no_follow to netflix (#579)
    * added no_follow to netflix
    
    * Update Footer.tsx
    
    added ` rel = noreferrer noopener `
    
    * linted the code

--- src/layouts/default/components/Footer.tsx
+++ src/layouts/default/components/Footer.tsx
@@ -65,7 +65,11 @@ const Footer: React.FunctionComponent = () => {
         />
       </FooterCopyright>
       <NetlifyIcon>
-        <a href="https://www.netlify.com" target="_blank">
+        <a
+          href="https://www.netlify.com"
+          target="_blank"
+          rel="nofollow noreferrer noopener"
+        >
           <img src={NetlifyImage} alt="deploys by Netlify" />
         </a>
       </NetlifyIcon>
