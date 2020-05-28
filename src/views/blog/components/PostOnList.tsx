e2eeb8bc9ec9a647713ec73645841528d246f25e
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2020-05-28 14:19:50 +0200

    Add read more button

--- src/views/blog/components/PostOnList.tsx
+++ src/views/blog/components/PostOnList.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Link from "@components/shared/Link";
+import Button from "@components/shared/Button";
 
 import PostHeader from "@views/blog/components/PostHeader";
 import PostContent from "@views/blog/components/content/PostContent";
 import PostFooter from "@views/blog/components/PostFooter";
 
 import { Post } from "@typings/blog";
+import { FormattedMessage } from "@common/i18n";
 
 import {
   PostOnListWrapper,
@@ -38,6 +40,11 @@ const PostOnList: React.FunctionComponent<Post> = ({
           forceDefaultTemplate={true}
         />
       </PostContentWrapper>
+      <Link.Internal to={fields.slug}>
+        <Button.Emphasized size="sm">
+          <FormattedMessage id="blog.readMoreButton" />
+        </Button.Emphasized>
+      </Link.Internal>
       <PostFooter tags={frontmatter.tags || []} />
     </PostOnListWrapper>
   );
