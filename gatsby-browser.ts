5fec670c085c901fd21215358830c3df381b130d
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2020-06-03 15:41:56 +0200

    .

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -3,11 +3,10 @@ import { browserAPI } from "./gatsby/browser-api";
 
 import "./static/css/fonts.css";
 import "./static/css/typography.css";
-import "./static/css/global.css";
-import "./static/css/docsearch.min.css";
-
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "@common/icons";
+import "./static/css/global.css";
+import "./static/css/docsearch.min.css";
 
 export const onInitialClientRender = browserAPI.onInitialClientRender;
 export const shouldUpdateScroll = browserAPI.shouldUpdateScroll;
