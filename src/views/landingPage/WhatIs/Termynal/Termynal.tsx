2be054cfc094ddaa5039a2a0b4afddc134f54423
Christian Berendt <berendt@betacloud-solutions.de>
2021-04-15 10:34:03 +0200

    Install minikube so that the getting started example works directly (#680)
    The getting started snippet on the landing page requires Minikube
    to be already installed.
    
    If this is not the case, the call to kyma provision minikube will
    fail.
    
    $ kyma provision minikube
    X Checking requirements
    Error: Executing the 'minikube version' command with output '' and
    error message 'exec: "minikube": executable file not found in $PATH'
    failed
    
    For the example to work directly, Minikube should also be installed.
    
    Signed-off-by: Christian Berendt <berendt@23technologies.cloud>

--- src/views/landingPage/WhatIs/Termynal/Termynal.tsx
+++ src/views/landingPage/WhatIs/Termynal/Termynal.tsx
@@ -4,7 +4,7 @@ import { TermynalStyles } from "./styled";
 export const Termynal: FC = () => (
   <TermynalStyles>
     <div id="termynal" data-termynal={true}>
-      <span data-ty="input">brew install kyma-cli</span>
+      <span data-ty="input">brew install kyma-cli minikube</span>
       <span data-ty="input">kyma provision minikube</span>
       <span data-ty="input">kyma install</span>
       <span data-ty={true}>Installation successful! Happy Kyma-ing :)</span>
