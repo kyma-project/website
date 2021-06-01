91a30c01338fbc7081c7bc33934e295434fe2faa
Marcin Witalis <marcin.witalis@sap.com>
2021-06-01 12:57:27 +0200

    test community hash checkout

--- tools/content-loader/src/prepare-community/index.ts
+++ tools/content-loader/src/prepare-community/index.ts
@@ -1,12 +1,11 @@
-import { resolve } from "path";
 import to from "await-to-js";
+import { resolve } from "path";
 import { VError } from "verror";
-
 import { CoreConfig, PrepareFor } from "../config";
-import communityConfig from "./config";
 import GitClient from "../github-client/git-client";
-import CopyCommunity from "./copy-community";
 import { makeDir } from "../helpers";
+import communityConfig from "./config";
+import CopyCommunity from "./copy-community";
 
 const prepareCommunity = async (coreConfig: CoreConfig) => {
   const outputPath = resolve(communityConfig.outputPath);
@@ -30,6 +29,12 @@ const prepareCommunity = async (coreConfig: CoreConfig) => {
       err,
       `while cloning ${coreConfig.organization}/${coreConfig.repository}`,
     );
+  }
+
+  const hash = "8c269a6c2e42da6ef92a57d8e04add7bcdbe8e90";
+  [err] = await to(GitClient.checkout(hash));
+  if (err) {
+    throw new VError(err, `while checkingout out ${hash}`);
   }
 
   [err] = await to(CopyCommunity.do(sourcePath, outputPath));
