acb101dd546c5a4a362da8bb27ef1f590846ef4d
Kwiatosz, Krzysztof <kwiatekus@users.noreply.github.com>
2021-09-27 13:42:56 +0200

    Changing kyma deploy commands

--- src/views/landingPage/CheckItOut/Termynal/Termynal.tsx
+++ src/views/landingPage/CheckItOut/Termynal/Termynal.tsx
@@ -4,8 +4,8 @@ import { TermynalStyles } from "./styled";
 export const Termynal: FC = () => (
   <TermynalStyles>
     <div id="termynal" data-termynal={true}>
-      <span data-ty="input">kyma provision minikube</span>
-      <span data-ty="input">kyma install -s 1.24.2</span>
+      <span data-ty="input">kyma provision k3d</span>
+      <span data-ty="input">kyma deploy</span>
     </div>
   </TermynalStyles>
 );
