1b8d47a286f58ec6412e5cce62634515dff7cf4f
Damian Badura <damian.badura@sap.com>
2021-09-30 13:39:17 +0200

    fix website

--- tools/content-loader/src/prepare-community/copy-community.ts
+++ tools/content-loader/src/prepare-community/copy-community.ts
@@ -1,12 +1,14 @@
 import to from "await-to-js";
 import { VError } from "verror";
 import { copy } from "fs-extra";
+import { join } from "path";
 
 export class CopyCommunity {
   do = async (source: string, output: string) => {
-    console.log(`Copy community content from ${source} to ${output}`);
+    const docsSrc = join(source, "docs");
+    console.log(`Copy community content from ${docsSrc} to ${output}`);
 
-    const [err] = await to(copy(source, output));
+    const [err] = await to(copy(docsSrc, output));
     if (err) {
       throw new VError(err, `while copying documentation to ${output}`);
     }
